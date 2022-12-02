eb60ae8c2ded3a5650afdb7209182d8a20c12729
Mathias Düsterhöft <mathias.duesterhoeft@moia.io>
2022-12-02 21:48:31 +0100

    day2 part2

--- src/day2.rs
+++ src/day2.rs
@@ -1,3 +1,4 @@
+use enum_iterator::{all, Sequence};
 use std::str::FromStr;
 
 pub struct Round {
@@ -7,22 +8,74 @@ pub struct Round {
 
 impl Round {
     fn score(&self) -> u32 {
-        self.win_score() + self.you.sign_score()
+        self.outcome().score() + self.you.sign_score()
     }
 
-    fn win_score(&self) -> u32 {
+    fn outcome(&self) -> Outcome {
         match self.you {
-            Sign::ROCK if self.opponent == Sign::SCISSORS => SCORE_WIN,
-            Sign::PAPER if self.opponent == Sign::ROCK => SCORE_WIN,
-            Sign::SCISSORS if self.opponent == Sign::PAPER => SCORE_WIN,
-            _ if self.you == self.opponent => SCORE_DRAW,
-            _ => SCORE_LOST,
+            Sign::ROCK if self.opponent.looses_against() == Sign::ROCK => Outcome::WIN,
+            Sign::PAPER if self.opponent.looses_against() == Sign::PAPER => Outcome::WIN,
+            Sign::SCISSORS if self.opponent.looses_against() == Sign::SCISSORS => Outcome::WIN,
+            _ if self.you == self.opponent => Outcome::DRAW,
+            _ => Outcome::LOOSE,
         }
     }
 }
 
-#[derive(PartialEq, Eq, Clone, Copy)]
-enum Sign {
+enum Outcome {
+    WIN,
+    LOOSE,
+    DRAW,
+}
+
+impl Outcome {
+    fn score(&self) -> u32 {
+        match self {
+            Self::WIN => 6,
+            Self::LOOSE => 0,
+            Self::DRAW => 3,
+        }
+    }
+}
+
+impl FromStr for Outcome {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        match input {
+            "X" => Ok(Outcome::LOOSE),
+            "Y" => Ok(Outcome::DRAW),
+            "Z" => Ok(Outcome::WIN),
+            _ => Err(()),
+        }
+    }
+}
+pub struct RoundPart2 {
+    opponent: Sign,
+    outcome: Outcome,
+}
+
+impl RoundPart2 {
+    fn score(&self) -> u32 {
+        let own_sign = self.own_sign();
+        let round = Round {
+            opponent: self.opponent,
+            you: own_sign,
+        };
+        round.outcome().score() + own_sign.sign_score()
+    }
+
+    fn own_sign(&self) -> Sign {
+        match self.outcome {
+            Outcome::DRAW => self.opponent,
+            Outcome::LOOSE => self.opponent.wins_against(),
+            Outcome::WIN => self.opponent.looses_against(),
+        }
+    }
+}
+
+#[derive(PartialEq, Eq, Clone, Copy, Debug, Sequence)]
+pub enum Sign {
     ROCK,
     PAPER,
     SCISSORS,
@@ -35,6 +88,24 @@ impl Sign {
             Self::PAPER => 2,
             Self::SCISSORS => 3,
         }
+    }
+
+    fn wins_against(&self) -> Sign {
+        match self {
+            Self::ROCK => Self::SCISSORS,
+            Self::PAPER => Self::ROCK,
+            Self::SCISSORS => Self::PAPER,
+        }
+    }
+
+    fn looses_against(&self) -> Self {
+        let wins = self.wins_against();
+        for sign in all::<Self>().collect::<Vec<_>>() {
+            if sign != wins && sign != *self {
+                return sign;
+            }
+        }
+        panic!("")
     }
 }
 
@@ -50,14 +121,42 @@ impl FromStr for Sign {
         }
     }
 }
-const SCORE_DRAW: u32 = 3;
-const SCORE_LOST: u32 = 0;
-const SCORE_WIN: u32 = 6;
 
-#[aoc_generator(day2)]
+impl FromStr for Round {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut split = input.split_whitespace();
+        let opponent = Sign::from_str(split.next().unwrap())?;
+        let you = Sign::from_str(split.next().unwrap())?;
+        Ok(Round { you, opponent })
+    }
+}
+
+impl FromStr for RoundPart2 {
+    type Err = ();
+
+    fn from_str(input: &str) -> Result<Self, Self::Err> {
+        let mut split = input.split_whitespace();
+        let opponent = Sign::from_str(split.next().unwrap())?;
+        let outcome = Outcome::from_str(split.next().unwrap())?;
+        Ok(RoundPart2 { opponent, outcome })
+    }
+}
+
+#[aoc_generator(day2, part1)]
 pub fn input_generator(input: &str) -> Vec<Round> {
     let lines = input.lines().collect::<Vec<&str>>();
-    lines.iter().map(|l| parse_line(l)).collect()
+    lines.iter().map(|l| Round::from_str(l).unwrap()).collect()
+}
+
+#[aoc_generator(day2, part2)]
+pub fn input_generator2(input: &str) -> Vec<RoundPart2> {
+    let lines = input.lines().collect::<Vec<&str>>();
+    lines
+        .iter()
+        .map(|l| RoundPart2::from_str(l).unwrap())
+        .collect()
 }
 
 #[aoc(day2, part1)]
@@ -65,16 +164,14 @@ pub fn solve_part1(input: &[Round]) -> u32 {
     input.iter().map(|r| r.score()).sum()
 }
 
-fn parse_line(line: &str) -> Round {
-    let mut split = line.split_whitespace();
-    let opponent = Sign::from_str(split.next().unwrap()).unwrap();
-    let you = Sign::from_str(split.next().unwrap()).unwrap();
-    Round { you, opponent }
+#[aoc(day2, part2)]
+pub fn solve_part2(input: &[RoundPart2]) -> u32 {
+    input.iter().map(|r| r.score()).sum()
 }
 
 #[cfg(test)]
 mod tests {
-    use super::{Round, Sign};
+    use super::{FromStr, Outcome, Round, RoundPart2, Sign};
     #[test]
     fn it_should_compute_sign_score() {
         assert_eq!(Sign::ROCK.sign_score(), 1);
@@ -88,8 +185,11 @@ mod tests {
             you: Sign::PAPER,
             opponent: Sign::ROCK,
         };
-        assert_eq!(round.win_score(), 6);
-        assert_eq!(round.score(), round.win_score() + round.you.sign_score());
+        assert_eq!(round.outcome().score(), 6);
+        assert_eq!(
+            round.score(),
+            round.outcome().score() + round.you.sign_score()
+        );
     }
 
     #[test]
@@ -98,8 +198,11 @@ mod tests {
             you: Sign::ROCK,
             opponent: Sign::ROCK,
         };
-        assert_eq!(round.win_score(), 3);
-        assert_eq!(round.score(), round.win_score() + round.you.sign_score());
+        assert_eq!(round.outcome().score(), 3);
+        assert_eq!(
+            round.score(),
+            round.outcome().score() + round.you.sign_score()
+        );
     }
 
     #[test]
@@ -108,7 +211,48 @@ mod tests {
             you: Sign::ROCK,
             opponent: Sign::PAPER,
         };
-        assert_eq!(round.win_score(), 0);
-        assert_eq!(round.score(), round.win_score() + round.you.sign_score());
+        assert_eq!(round.outcome().score(), 0);
+        assert_eq!(
+            round.score(),
+            round.outcome().score() + round.you.sign_score()
+        );
+    }
+
+    #[test]
+    fn it_should_parse_sign() {
+        let line = "A X";
+        let round = Round::from_str(line).unwrap();
+        assert_eq!(round.you, Sign::ROCK);
+        assert_eq!(round.opponent, Sign::ROCK);
+    }
+
+    #[test]
+    fn it_score_round_2_loose() {
+        let line = "A X";
+        let round = RoundPart2::from_str(line).unwrap();
+        assert_eq!(
+            round.score(),
+            Outcome::LOOSE.score() + Sign::SCISSORS.sign_score()
+        );
+    }
+
+    #[test]
+    fn it_score_round_2_win() {
+        let line = "B Z";
+        let round = RoundPart2::from_str(line).unwrap();
+        assert_eq!(
+            round.score(),
+            Outcome::WIN.score() + Sign::SCISSORS.sign_score()
+        );
+    }
+
+    #[test]
+    fn it_score_round_2_draw() {
+        let line = "C Y";
+        let round = RoundPart2::from_str(line).unwrap();
+        assert_eq!(
+            round.score(),
+            Outcome::DRAW.score() + Sign::SCISSORS.sign_score()
+        );
     }
 }
